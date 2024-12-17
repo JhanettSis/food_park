70c6abba9857b401fba9ce05c95f7921ec9ac569
Jhanett <jhanettsis@gmail.com>
2024-12-17 20:35:10 +0000

    Frontend Panel Mastering without yield

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [FrontendController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
