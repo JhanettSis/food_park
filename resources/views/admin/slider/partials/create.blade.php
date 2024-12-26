3e1aa2072aab71137ef5e7fd05f76a14c798b789
Jhanett <jhanettsis@gmail.com>
2024-12-26 23:03:43 +0000

    Create form insert view template for sliders and adding image input

---
+++ resources/views/admin/slider/partials/create.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.master')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Silder</h1>
+        </div>
+
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>Create new Slider</h4>
+
+            </div>
+            <div class="card-body">
+                <form action="">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Thumbnail</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <div id="image-preview" class="image-preview">
+                                        <label for="image-upload" id="image-label">Choose File</label>
+                                        <input type="file" name="image" id="image-upload" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Offer</label>
+                                <input type="text" name="offer" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Status</label>
+                                <select name="status" class="form-control">
+                                    <option value="1">Yes</option>
+                                    <option value="0">No</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <div class="form-group">
+                                <label>Title</label>
+                                <input type="text" name="title" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Subtitle</label>
+                                <input type="text" name="subtitle" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea name="short_description" rows="4" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Button link</label>
+                                <input type="text" name="button_link" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
